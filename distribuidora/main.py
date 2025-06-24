8fb5378051bec93755302d0912d9e8e48c493fca
Augusto Leal <augustoleal11@gmail.com>
2025-06-24 20:32:38 -0300

    atualização do fake_data para a tabela entrega

--- distribuidora/main.py
+++ distribuidora/main.py
@@ -14,28 +14,28 @@ def menu():
 if __name__ == '__main__':
     generate_data()
 
-    pessoa1 = Pessoa() # cria um objeto Pessoa
-    #exibindo a pessoa de id 1
-    pessoa1.carregar(1) #chama o metodo carregar para carregar os dados da pessoa com ID 1
-    print(pessoa1.nome)
-    print(pessoa1.email)
-    print(pessoa1.cpf)
-    print(pessoa1.data_nascimento)
-
-    # Atualizando os dados da pessoa de id 1
-    pessoa1.nome = "João Silva"
-    pessoa1.email = "joaozinho@gmail.com"
-    pessoa1.cpf = "12345699912"
-    pessoa1.data_nascimento = "1990-01-01"
-    pessoa1.salvar() # chama o metodo salvar para atualizar os dados no BANCO DE DADOS da pessoa com ID 1
-
-    # Exibindo os dados atualizados da pessoa de id 1
-    print(pessoa1.nome)
-    print(pessoa1.email)
-    print(pessoa1.cpf)
-    print(pessoa1.data_nascimento)
-
-    # # Deletando a pessoa de id 1
-    pessoa1.deletar() # chama o metodo deletar para deletar os dados da pessoa com ID 1
+    # pessoa1 = Pessoa() # cria um objeto Pessoa
+    # #exibindo a pessoa de id 1
+    # pessoa1.carregar(1) #chama o metodo carregar para carregar os dados da pessoa com ID 1
+    # print(pessoa1.nome)
+    # print(pessoa1.email)
+    # print(pessoa1.cpf)
+    # print(pessoa1.data_nascimento)
+    #
+    # # Atualizando os dados da pessoa de id 1
+    # pessoa1.nome = "João Silva"
+    # pessoa1.email = "joaozinho@gmail.com"
+    # pessoa1.cpf = "12345699912"
+    # pessoa1.data_nascimento = "1990-01-01"
+    # pessoa1.salvar() # chama o metodo salvar para atualizar os dados no BANCO DE DADOS da pessoa com ID 1
+    #
+    # # Exibindo os dados atualizados da pessoa de id 1
+    # print(pessoa1.nome)
+    # print(pessoa1.email)
+    # print(pessoa1.cpf)
+    # print(pessoa1.data_nascimento)
+    #
+    # # # Deletando a pessoa de id 1
+    # pessoa1.deletar() # chama o metodo deletar para deletar os dados da pessoa com ID 1
 
     close_connection()
